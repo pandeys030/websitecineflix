cc08b1e0f3470d98d5c7c4bc7eda609aabdbc6b3
Swarali Bhalerao <75970656+Swarali-31@users.noreply.github.com>
2024-11-09 18:08:21 +0530

    Update MovieCard.js

--- movie-frontend/src/components/MovieCard.js
+++ movie-frontend/src/components/MovieCard.js
@@ -1,19 +1,18 @@
-// src/components/MovieCard.js
-
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import './MovieCard.css'; // Make sure to create/modify this file accordingly.
+import './MovieCard.css';
 
 const MovieCard = ({ movie }) => {
   const navigate = useNavigate();
 
   const goToDetails = () => {
-    navigate(`/movies/${movie.id}`);
+    navigate(`/movies/${movie._id}`);
   };
 
   return (
     <div className="movie-card" onClick={goToDetails}>
-      <img className="movie-poster" src={movie.poster} alt={movie.title} />
+      {/* Use the poster field from the movie object as the image source */}
+      <img className="movie-poster" src={movie.poster || "/path/to/default-image.jpg"} alt={movie.title} />
       <div className="movie-title">{movie.title}</div>
     </div>
   );
