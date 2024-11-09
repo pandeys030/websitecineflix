eb951e9acf852b77650236edd4a21689d02eca26
Swarali Bhalerao <75970656+Swarali-31@users.noreply.github.com>
2024-11-09 18:05:50 +0530

    Update movie.js

--- movie-frontend/backend/models/movie.js
+++ movie-frontend/backend/models/movie.js
@@ -1,24 +1,17 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
+
+// Define the schema for the movies collection
 const movieSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-    },
-    director: {
-        type: String,
-        required: true,
-    },
-    year: {
-        type: Number,
-        required: true,
-    },
-    genre: {
-        type: String,
-        required: true,
-    }
-});
+  title: { type: String, required: true },
+  poster: { type: String, required: true },
+  releaseDate: { type: String, required: true },
+  genre: { type: [String], required: true },
+  rating: { type: String, required: true },
+  director: { type: String, required: true },
+  description: { type: String, required: true },
+  trailerUrl: { type: String, required: true }
+}, { timestamps: true });
 
-const Movie = mongoose.model('Movie', movieSchema);
-
+const Movie = mongoose.model('Movie', movieSchema);  // Create the Movie model
 module.exports = Movie;
