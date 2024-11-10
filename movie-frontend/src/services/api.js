e4411103a1882481239c6fa71c08c9770e17f886
Swarali Bhalerao <75970656+Swarali-31@users.noreply.github.com>
2024-11-10 14:49:33 +0530

    Update api.js

--- movie-frontend/src/services/api.js
+++ movie-frontend/src/services/api.js
@@ -4,9 +4,20 @@ const API_URL = "http://localhost:3001/api";
 
 // Movies API
 export const getMovies = () => axios.get(`${API_URL}/movies`);
-export const getMovieById = (id) => axios.get(`${API_URL}/movies/${id}`);
+export const getMovieById = (movieId) => axios.get(`${API_URL}/movies/${movieId}`);
 
 // My List API
-export const getMyList = () => axios.get(`${API_URL}/myList`);
-export const addToMyList = (id) => axios.post(`${API_URL}/myList/${id}`);
-export const removeFromMyList = (id) => axios.delete(`${API_URL}/myList/${id}`);
+export const getMyList = () => axios.get(`${API_URL}/my-list`);
+export const addToMyList = async (movieId, title) => {
+  try {
+    const response = await axios.post(`${API_URL}/my-list`, { movieId, title });
+    return response.data;
+  } catch (error) {
+    if (error.response && error.response.status === 400) {
+      return { message: 'Movie is already in My List' };
+    }
+    throw error; // Handle other errors as needed
+  }
+};
+
+export const removeFromMyList = (movieId) => axios.delete(`${API_URL}/my-list/${movieId}`);
