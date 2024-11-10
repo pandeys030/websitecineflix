101e0940185adb6cd3e08d5e89631d46cc7242c3
Swarali Bhalerao <75970656+Swarali-31@users.noreply.github.com>
2024-11-10 14:47:12 +0530

    Update MovieCard.js

--- movie-frontend/src/components/MovieCard.js
+++ movie-frontend/src/components/MovieCard.js
@@ -1,3 +1,48 @@
+// // src/components/MovieCard.js
+
+// import React from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import './MovieCard.css'; // Make sure to create/modify this file accordingly.
+
+// const MovieCard = ({ movie }) => {
+//   const navigate = useNavigate();
+
+//   const goToDetails = () => {
+//     navigate(`/movies/${movie.id}`);
+//   };
+
+//   return (
+//     <div className="movie-card" onClick={goToDetails}>
+//       <img className="movie-poster" src={movie.poster} alt={movie.title} />
+//       <div className="movie-title">{movie.title}</div>
+//     </div>
+//   );
+// };
+
+// export default MovieCard;
+
+
+// import React from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import './MovieCard.css'; // Make sure to create/modify this file accordingly.
+
+// const MovieCard = ({ movie }) => {
+//   const navigate = useNavigate();
+
+//   const goToDetails = () => {
+//     // Use movie._id instead of movie.id to match MongoDB's ObjectId
+//     navigate(`/movies/${movie._id}`);
+//   };
+
+//   return (
+//     <div className="movie-card" onClick={goToDetails}>
+//       <img className="movie-poster" src={movie.posterUrl} alt={movie.title} />
+//       <div className="movie-title">{movie.title}</div>
+//     </div>
+//   );
+// };
+
+// export default MovieCard;
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './MovieCard.css';
@@ -11,7 +56,6 @@ const MovieCard = ({ movie }) => {
 
   return (
     <div className="movie-card" onClick={goToDetails}>
-      {/* Use the poster field from the movie object as the image source */}
       <img className="movie-poster" src={movie.poster || "/path/to/default-image.jpg"} alt={movie.title} />
       <div className="movie-title">{movie.title}</div>
     </div>
