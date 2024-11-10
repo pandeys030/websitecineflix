b22dc51980f79b8cc71d430cfc208234d941ccdd
Swarali Bhalerao <75970656+Swarali-31@users.noreply.github.com>
2024-11-10 14:39:26 +0530

    Update myList.js

--- movie-frontend/backend/models/myList.js
+++ movie-frontend/backend/models/myList.js
@@ -1,11 +1,11 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
 
-
-// Define the schema for the MyList collection
 const myListSchema = new mongoose.Schema({
-  movieId: { type: mongoose.Schema.Types.ObjectId, ref: 'Movie', required: true }, // Link to a movie document
-  addedAt: { type: Date, default: Date.now } // Timestamp for when the movie was added to My List
+  title: {
+    type: String,
+    required: true,
+    unique: true // To prevent duplicate titles
+  }
 });
 
-const MyList = mongoose.model('MyList', myListSchema);  // Create the MyList model
-module.exports = MyList;
+module.exports = mongoose.model('MyList', myListSchema, 'myList');
