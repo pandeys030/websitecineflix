d3dd18c015cf2aa36959652f7e96b44387148b2a
Swarali Bhalerao <75970656+Swarali-31@users.noreply.github.com>
2024-11-13 19:28:48 +0530

    Update server.js

--- movie-frontend/backend/server.js
+++ movie-frontend/backend/server.js
@@ -1,27 +1,4 @@
-// // server.js
-// const cors = require('cors');
-// const express = require('express');
-// const connectDB = require('./config/db.js'); // Default import for connectDB
-// const movieRoutes = require('./routes/movieRoutes.js'); // Import movie routes
-// const myListRoutes = require('./routes/myListRoutes.js'); // Import my list routes
 
-// const app = express();
-
-// // Middleware
-// app.use(cors()); 
-// app.use(express.json()); 
-
-// // API Routes
-// app.use('/api/movies', movieRoutes); // Route for movie-related actions
-// app.use('/api/my-list', myListRoutes); // Route for My List actions
-
-// // Connect to MongoDB
-// connectDB();
-
-// // Start the server
-// app.listen(3001, () => {
-//   console.log('Server is running on port 3001');
-// });
 const cors = require('cors');
 const express = require('express');
 const connectDB = require('./config/db.js'); // Default import for connectDB
